a885b072631d6781a36092916c3dba5573d2497c
Yafa Yusufova <yafulia@mail.ru>
2017-08-07 13:45:32 +0300

    Доработала

--- find_procedure.py
+++ find_procedure.py
@@ -5,31 +5,25 @@ migrations = 'Migrations'
 current_dir = os.path.dirname(os.path.abspath(__file__))
 
 
-def def_files_with_string(files):
-	print('Введите строку:', end=' ')
-	s = input()
+def files_with_string(files):
+	s = input('Введите строку: ')
 	
 	sql_files_true = []
-	
 	for sql_file in files:
-		with open(os.path.join(*[current_dir, migrations], sql_file)) as f:
-			for line in f:
-				if s in line:
-					sql_files_true.append(sql_file)
-					break
-			else:
-				files.remove(sql_file)
+		with open(os.path.join(current_dir, migrations, sql_file)) as f:
+			if f.read().find(s) != -1:
+				sql_files_true.append(sql_file)
+				
 	return sql_files_true
 
 
 if __name__ == '__main__':
-	all_files = os.listdir(os.path.join(current_dir, migrations))
-	sql_files = [file for file in all_files if file.split('.')[-1] == 'sql']
+	sql_files = [file for file in os.listdir(os.path.join(current_dir, migrations))
+		if os.path.splitext(file)[1] == '.sql']
 	
 	while True:
-		files_to_print = def_files_with_string(sql_files)
-					
-		for file_to_print in files_to_print:
-			print(file_to_print)
+		sql_files = files_with_string(sql_files)
+		for sql_file in sql_files:
+			print(sql_file)
 			
-		print('Всего:', len(files_to_print))
+		print('Всего:', len(sql_files))
